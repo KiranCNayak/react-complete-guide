83ab1684f4631e4e298a11210fedea47a0ec4b2f
KiranCNayak <kiran.nayak504@gmail.com>
2020-07-12 21:47:56 +0530

    Added proptypes as a dependency and added it in Person/index.js

--- src/components/Persons/Person/index.js
+++ src/components/Persons/Person/index.js
@@ -1,4 +1,5 @@
 import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
 import classes from './Person.css';
 import Aux from '../../../hoc/Aux';
 import withClass from '../../../hoc/withClass';
@@ -12,15 +13,26 @@ class Person extends PureComponent {
     return (
       // <div className={classes.Person} onClick={this.props.clicked}>
       <Aux>
-        <p>Hi! I'm {this.props.name}</p>
+        <p onClick={this.props.clicked}>Hi! I'm {this.props.name}</p>
         <p>
           I'm {this.props.age} {this.props.age === '1' ? 'year' : 'years'} old
         </p>
-        <input type='text' onChange={this.props.changed} value={this.props.name}/>
+        <input
+          type='text'
+          onChange={this.props.changed}
+          value={this.props.name}
+        />
       </Aux>
       // </div>
     );
   }
 }
 
-export default withClass(Person, classes.Person );
+Person.propTypes = {
+  click: PropTypes.func,
+  name: PropTypes.string,
+  age: PropTypes.number,
+  changed: PropTypes.func
+};
+
+export default withClass(Person, classes.Person);
